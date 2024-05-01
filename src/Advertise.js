c82f3890be4bd283702547e9d8cde6c5a380d3dd
klu_2200031618 <kk258786@gmail.com>
2024-05-01 16:57:00 +0530

    The First with animation is set and need to add scroll on animation

--- src/Advertise.js
+++ src/Advertise.js
@@ -2,9 +2,13 @@ import './Advertise.css';
 function App() {
   return (
     <div className="main-background">
-      <div className='product-container'>
-        <img src={require("./images/product.png")} className='product' alt='product'/>
-      </div>
+      <center><div className='product-container-one'>
+        <img src={require("./images/productFull.png")} className='product' alt='product'/>
+        <h1 className='product-name'>Iphone 12 pro</h1>
+      </div></center><br></br>
+      <center><div className='product-container-two'>
+        {/* <img src={require("./images/productFull.png")} className='product' alt='product'/> */}
+      </div></center>
     </div>
   );
 }
