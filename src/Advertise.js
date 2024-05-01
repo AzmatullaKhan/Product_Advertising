6babae0e0b5ad5dc51129ec5c32fd7f10cdbbbea
klu_2200031618 <kk258786@gmail.com>
2024-05-01 14:18:28 +0530

    Initial commit(Product addition, Product animation is done)

---
+++ src/Advertise.js
@@ -0,0 +1,12 @@
+import './Advertise.css';
+function App() {
+  return (
+    <div className="main-background">
+      <div className='product-container'>
+        <img src={require("./images/product.png")} className='product' alt='product'/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
