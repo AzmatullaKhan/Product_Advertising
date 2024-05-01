8443b3de9e20966fe93571375938a559d6b588c3
klu_2200031618 <kk258786@gmail.com>
2024-05-01 20:59:04 +0530

    Entire Project advertisement has been completed

--- src/Advertise.js
+++ src/Advertise.js
@@ -1,14 +1,67 @@
 import './Advertise.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import React, {useEffect} from 'react';
+
+
 function App() {
+
+  useEffect(()=>{
+    AOS.init({duration:2000})
+  }, [])
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
   return (
     <div className="main-background">
-      <center><div className='product-container-one'>
-        <img src={require("./images/productFull.png")} className='product' alt='product'/>
-        <h1 className='product-name'>Iphone 12 pro</h1>
-      </div></center><br></br>
-      <center><div className='product-container-two'>
-        {/* <img src={require("./images/productFull.png")} className='product' alt='product'/> */}
-      </div></center>
+      <section data-aos="fade-right" data-aos-offset="100">
+        <div className='product-container-one'>
+          <img src={require("./images/productFull.png")} className='product-img1' alt='product'/>
+          <h1 className='product-name'>Iphone 12 pro</h1>
+        </div>
+      </section>
+      <section data-aos="fade-left" data-aos-offset="300" style={{height:"80vh"}}>
+        <div className='product-container-two' data-aos="fade-left" data-aos-offset="300">
+          <img src={require("./images/productTop.png")} className='product-img2' alt='product'/>
+          <div className='product-two-description' data-aos="fade-left" data-aos-offset="300">
+            <h1>Titanium Body</h1>
+            <p>Apple's exploration into utilizing titanium for iPhone construction signifies a departure from traditional materials like aluminum and stainless steel. Renowned for its exceptional strength and lightweight properties, titanium promises enhanced durability and a premium feel. With its corrosion resistance and sleek aesthetic, a titanium-bodied iPhone could set a new standard for luxury and resilience in smartphone design. This material choice aligns with Apple's commitment to innovation and quality, potentially redefining the user experience for iPhone enthusiasts.</p>
+          </div>
+        </div>
+      </section>
+      <section data-aos="fade-right" data-aos-offset="300" style={{height:"80vh",display:"flex"}}>
+          <div className='product-three-description'>
+              <h1 style={{textDecoration:"underline"}}>Key Specs</h1>
+              <p>Front-Facing Camera: 12 megapixels</p>
+              <p>Rear-Facing Camera: 12 megapixels</p>
+              <h1 style={{textDecoration:"underline"}}>Display</h1>
+              <p>Screen Size: 6.1 inches</p>
+              <p>Screen Resolution: 2532 x 1170</p>
+              <p>Screen Type: Super Retina XDR display</p>
+              <h1 style={{textDecoration:"underline"}}>Processor</h1>
+              <p>SOperating System :Apple iOS 14</p>
+              <p>Processor Model: A14 Bionic chip with next-generation Neural Engine</p>
+              <h1 style={{textDecoration:"underline"}}>Available In</h1>
+              <p>128gb rom, 8gb ram---1,20,000₹</p>
+              <p>256gb rom, 8gb ram---1,40,000₹</p>
+          </div> 
+        <div className='product-container-three' data-aos="fade-right" data-aos-offset="300">
+          <img src={require("./images/productCamera.png")} alt="Cameras"/>
+        </div>
+      </section>
+      <section data-aos="fade-left" data-aos-offset="300" style={{height:"80vh", display:"flex", justifyContent:"center"}}>
+        <div className='product-container-four' data-aos="fade-left">
+          <img src={require("./images/productColors.png")} alt="colors" style={{height:"400px", width:"400px"}}/>
+        </div>
+        <div className='product-four-description'data-aos="fade-left">
+        <h1 style={{textDecoration:"underline"}}>Colors Available</h1>
+              <p style={{color:"#89b7ce", fontWeight:"bold"}}>⦾ Metal Blue</p>
+              <p style={{color:"#fef2dd", fontWeight:"bold"}}>⦾ Gold</p>
+              <p style={{color:"#aaaba1", fontWeight:"bold"}}>⦾ Gray</p>
+              <p style={{color:"#fffff8", fontWeight:"bold"}}>⦾ White</p>
+
+        </div>
+      </section>
     </div>
   );
 }
